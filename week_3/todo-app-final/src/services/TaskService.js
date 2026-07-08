9dea6cf7f7b6587c6e9d9dad379070da86e60435
Sandesh Basnet <sanddy.basnet@gmail.com>
2026-07-08 09:46:41 +0545

    Complete week2 and week3

---
+++ week_3/todo-app-final/src/services/TaskService.js
@@ -0,0 +1,20 @@
+import localforage from "localforage";
+
+const taskListKey = "taskList";
+
+export async function getTasks() {
+  let taskList = await localforage.getItem(taskListKey);
+  return taskList ?? [];
+}
+
+export async function createTask(title, deadline, isUrgent) {
+  let taskObj = {
+    text: title,
+    time: deadline,
+    isUrgent,
+  };
+  let taskList = await localforage.getItem(taskListKey);
+  taskList = taskList ?? [];
+  taskList = [taskObj, ...taskList];
+  await localforage.setItem(taskListKey, taskList);
+}
